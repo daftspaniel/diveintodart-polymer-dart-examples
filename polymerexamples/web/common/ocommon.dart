7127aa14d73c44891901e4b707ce5f0a06147f43
Davy Mitchell <daftspaniel@gmail.com>
2013-09-25 21:35:46 +0100

    This is no longer required. Header is now custom element.

--- polymerexamples/web/common/ocommon.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:html';
-var DIDLink = "<a href=\"http://divingintodart.blogspot.co.uk/?pes\">Visit the 'Diving Into Dart' Blog</a>";
-WriteHeader(){query("#BlogLink").innerHtml = DIDLink;}
